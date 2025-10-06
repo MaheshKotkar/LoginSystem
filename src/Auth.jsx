590be377bcbd6c491242c54e25c7d0de09dac3f2
MaheshKotkar <maheshkotkar2005@gmail.com>
2025-10-06 16:30:52 +0530

    Update Auth.jsx

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -52,7 +52,7 @@ function Auth() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('https://loginsystembackend-eeqx.onrender.com/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
